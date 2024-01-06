b1d606c31c41d872a05a83725b9e34ce6d12632e
kawishika <hnkmalwenna@gmail.com>
2024-01-06 15:38:40 +0530

    Optimized

--- app/src/main/java/com/example/diceroller/DiceViewModel.kt
+++ app/src/main/java/com/example/diceroller/DiceViewModel.kt
@@ -9,25 +9,28 @@ import kotlinx.coroutines.flow.update
 
 class DiceViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(DiceUiState(R.drawable.dice_1))
-    val uiState:StateFlow<DiceUiState> = _uiState.asStateFlow()
+    val uiState: StateFlow<DiceUiState> = _uiState.asStateFlow()
 
     init {
-        Log.d("View Model","init Called !")
+        Log.d("View Model", "init Called !")
         Logic()
     }
+
     fun Logic() {
-        Log.d("View Model","Button Clicked !")
+        Log.d("View Model", "Button Clicked !")
         val random = (1..6).random()
-        val resource = when(random){
-            1-> R.drawable.dice_1
-            2-> R.drawable.dice_2
-            3-> R.drawable.dice_3
-            4-> R.drawable.dice_4
-            5-> R.drawable.dice_5
-            else-> R.drawable.dice_6
+        val resource = when (random) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
-        _uiState.update { diceUiState -> diceUiState.copy(
-            imageResource = resource
-        ) }
+        _uiState.update { diceUiState ->
+            diceUiState.copy(
+                imageResource = resource
+            )
+        }
     }
 }
--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,29 +3,6 @@ package com.example.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
-import androidx.compose.ui.unit.dp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
